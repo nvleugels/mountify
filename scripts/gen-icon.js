8ed9a2037e02317fb3fdfebed3455ce6ea5f0d21
Niek Vleugels <niek.vleugels@gmail.com>
2026-01-20 15:09:16 +0100

    Add png-to-ico icon generator and CI signing/decode steps

--- scripts/gen-icon.js
+++ scripts/gen-icon.js
@@ -1,22 +1,21 @@
-const { exec } = require('child_process');
+const fs = require('fs');
 const path = require('path');
+const pngToIco = require('png-to-ico');
 
 const src = path.join(__dirname, '..', 'images', 'mountify.png');
 const dest = path.join(__dirname, '..', 'images', 'mountify.ico');
 
-// Use ImageMagick `magick` to auto-resize and create .ico
-const cmd = `magick "${src}" -define icon:auto-resize=256,128,64,48,32,16 "${dest}"`;
+if (!fs.existsSync(src)) {
+  console.error('Source PNG not found:', src);
+  process.exit(1);
+}
 
-console.log('Running:', cmd);
-exec(cmd, (err, stdout, stderr) => {
-  if (err) {
-    console.error('Failed to generate .ico automatically.');
-    console.error('Error:', err.message);
-    if (stderr) console.error(stderr);
-    console.log('If you do not have ImageMagick installed, you can create the .ico manually with:');
-    console.log(`magick ${src} -define icon:auto-resize=256,128,64,48,32,16 ${dest}`);
+pngToIco(src)
+  .then((buf) => {
+    fs.writeFileSync(dest, buf);
+    console.log('Created ICO at', dest);
+  })
+  .catch((err) => {
+    console.error('Failed to create ICO:', err);
     process.exit(1);
-  }
-  console.log('mountify.ico created at', dest);
-  console.log(stdout);
-});
+  });
