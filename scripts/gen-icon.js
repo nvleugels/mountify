1ffbe06279b7284480af44c8dc85409ca7f3a82e
Niek Vleugels <niek.vleugels@gmail.com>
2026-01-20 15:17:23 +0100

    Remove scripts/gen-icon.js file

--- scripts/gen-icon.js
+++ scripts/gen-icon.js
@@ -1,21 +1,21 @@
-const fs = require('fs');
-const path = require('path');
-const pngToIco = require('png-to-ico');
+const fs = require("fs");
+const path = require("path");
+const pngToIco = require("png-to-ico");
 
-const src = path.join(__dirname, '..', 'images', 'mountify.png');
-const dest = path.join(__dirname, '..', 'images', 'mountify.ico');
+const src = path.join(__dirname, "..", "images", "mountify.png");
+const dest = path.join(__dirname, "..", "images", "mountify.ico");
 
 if (!fs.existsSync(src)) {
-  console.error('Source PNG not found:', src);
+  console.error("Source PNG not found:", src);
   process.exit(1);
 }
 
 pngToIco(src)
   .then((buf) => {
     fs.writeFileSync(dest, buf);
-    console.log('Created ICO at', dest);
+    console.log("Created ICO at", dest);
   })
   .catch((err) => {
-    console.error('Failed to create ICO:', err);
+    console.error("Failed to create ICO:", err);
     process.exit(1);
   });
